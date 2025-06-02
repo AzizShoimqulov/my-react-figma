522e4d4133db4383100f0659cdbbcdb6bce6aaf6
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-06-02 17:45:02 +0500

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
   return (
-    <>
-        <nav className=''>
-
+        <nav className='shadow-md pl-25 pr-25 pt-4 pb-4 bg-[#020817] mx-auto items-center border-b border[#101829] text-[#F8FAFC]'>
+            
+        <div className="flex justify-between">
+            <div className='flex gap-8'>
+            <h1>Store</h1>
+            <a href="#" className="text-[#F8FAFC] hover:text-blue-600">
+                Home
+            </a>
+            <a href="#" className="text-[#F8FAFC] hover:text-blue-600">
+              Product
+            </a>
+            <a href="#" className="text-[#F8FAFC] hover:text-blue-600">
+              Categories
+            </a>
+            </div>
+          <div className="hidden md:flex items-center space-x-4">
+          <LuSun />
+          </div>
+          </div>
         </nav>
-    </>
   )
 }
 
