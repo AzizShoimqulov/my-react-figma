fa2c6adb6f30063e53060d543b2fc08eb1325602
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-06-03 21:26:21 +0500

    add

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // yoki loyihangizdagi fayl joylashuvi
+  ],
+  darkMode: 'class', // ⚠️ kerakli sozlama
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
