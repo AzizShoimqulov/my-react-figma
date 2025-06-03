73ae529b2a9217efc21dcc5549a53a72206b4be3
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-06-03 12:56:50 +0500

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,48 @@
-import React from 'react'
+import React from 'react';
 import { LuSun } from "react-icons/lu";
+import { FaRegHeart } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { IoPersonCircleSharp } from "react-icons/io5";
 
+  const navItems = [
+    { id: 1, name: "Home", link: "/" },
+    { id: 2, name: "Product", link: "/" },
+    { id: 3, name: "Categories", link: "/" }
+  ];
+  const navIcons = [
+  { id: 1, icon: <LuSun size={18} /> },
+  { id: 2, icon: <FaRegHeart size={18} /> },
+  { id: 3, icon: <FiShoppingCart size={18} /> },
+  { id: 4, icon: <IoPersonCircleSharp size={21} /> },
+];
 const Navbar = () => {
   return (
-        <nav className='shadow-md pl-25 pr-25 pt-4 pb-4 bg-[#020817] mx-auto items-center border-b border[#101829] text-[#F8FAFC]'>
-            
-        <div className="flex justify-between">
-            <div className='flex gap-8'>
-            <h1>Store</h1>
-            <a href="#" className="text-[#F8FAFC] hover:text-blue-600">
-                Home
-            </a>
-            <a href="#" className="text-[#F8FAFC] hover:text-blue-600">
-              Product
-            </a>
-            <a href="#" className="text-[#F8FAFC] hover:text-blue-600">
-              Categories
-            </a>
-            </div>
-          <div className="hidden md:flex items-center space-x-4">
-          <LuSun />
+    <nav className="w-full border-b-4 border-b-blue-900 bg-gray-900 text-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+       
+          <div className="flex items-center gap-8">
+            <h1 className="text-xl font-bold">Store</h1>
+        {navItems.map(item => (
+          <div key={item.id}>
+             <a className='hover:text-blue-400 transition-colors duration-200' to={item.link}>{item.name}</a>
           </div>
+        ))}
           </div>
-        </nav>
-  )
-}
 
-export default Navbar
+        <div className="flex items-center gap-8">
+           {navIcons.map(item => (
+            <button
+                  key={item.id}
+                  className="text-white hover:text-yellow-400 transition-colors duration-200">
+                  {item.icon}
+            </button>
+            ))}
+        </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
